831c919ba182b43fc1cedbf357de5f97575ae757
Matt Wills <mattwillscode@gmail.com>
2021-06-15 22:35:27 -0400

    fix out-of-bounds memory bug

--- ponza_sim.cpp
+++ ponza_sim.cpp
@@ -19,33 +19,36 @@ enum card {
   MOUNTAIN,
   STOMPING_GROUND,
   FETCHLAND,
-  COLORLESS_LAND
+  COLORLESS_LAND,
+  FILLER,
 };
 
-// Configuration
-const int DECK_SIZE = 60;
-const int STARTING_HAND_SIZE = 7;
-const int MAX_MULLIGANS = 3;
-const int NUM_TRIALS = 100000;
-map<card, int> decklist =
-{
-    { card::ARBOR_ELF, 4 },
-    { card::UTOPIA_SPRAWL, 4 },
-    { card::BIRDS_OF_PARADISE, 2 },
-    { card::PILLAGE, 4 },
-    { card::BLOOD_MOON, 4 },
-    { card::MAGUS_OF_THE_MOON, 2 },
-    { card::FOREST, 7 },
-    { card::MOUNTAIN, 1 },
-    { card::STOMPING_GROUND, 3 },
-    { card::FETCHLAND, 8 },
-    { card::COLORLESS_LAND, 1 },
+class Configuration {
+  public:
+    const int DECK_SIZE = 60;
+    const int STARTING_HAND_SIZE = 7;
+    const int MAX_MULLIGANS = 3;
+    const int NUM_TRIALS = 10000;
+    const map<card, int> DECKLIST =
+    {
+      { card::ARBOR_ELF, 4 },
+      { card::UTOPIA_SPRAWL, 4 },
+      { card::BIRDS_OF_PARADISE, 2 },
+      { card::PILLAGE, 4 },
+      { card::BLOOD_MOON, 4 },
+      { card::MAGUS_OF_THE_MOON, 2 },
+      { card::FOREST, 7 },
+      { card::MOUNTAIN, 1 },
+      { card::STOMPING_GROUND, 3 },
+      { card::FETCHLAND, 8 },
+      { card::COLORLESS_LAND, 1 },
+    };
 };
 
 // Returns an unshuffled deck of DECK_SIZE containing the cards from config
-vector<char> initializeDeck()
+vector<card> initializeDeck(map<card, int> decklist, int deckSize)
 {
-  vector<char> deck;
+  vector<card> deck;
 
   for (auto const& x : decklist) {
     for (int i=0; i < x.second; i++) {
@@ -53,19 +56,19 @@ vector<char> initializeDeck()
     }
   }
 
-  while (deck.size() < DECK_SIZE) {
-    deck.push_back(' ');
+  while (deck.size() < (unsigned)deckSize) {
+    deck.push_back(card::FILLER);
   }
 
   return deck;
 }
 
 // Returns true if the given hand will allow a turn-2 land destruction spell
-bool verifyHand(vector<char> hand)
+bool verifyHand(vector<card> hand)
 {
   map<char,int> count;
-  for (int i= 0; i < STARTING_HAND_SIZE; ++i) {
-    count[hand[i]]++;
+  for (auto const card : hand) {
+    count[card]++;
   }
 
   const bool isRampSpellAvailable = (
@@ -120,43 +123,43 @@ void runTests()
 {
   cout << "Running tests..." << '\n';
 
-  vector<char> test1 = { card::FOREST, card::ARBOR_ELF, card::STOMPING_GROUND, card::BLOOD_MOON };
+  vector<card> test1 = { card::FOREST, card::ARBOR_ELF, card::STOMPING_GROUND, card::BLOOD_MOON };
   (verifyHand(test1) == true) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "arbor elf + blood moon + forest + stomping ground\n";
 
-  vector<char> test2 = { card::FOREST, card::ARBOR_ELF };
+  vector<card> test2 = { card::FOREST, card::ARBOR_ELF };
   (verifyHand(test2) == false) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "arbor elf + forest\n";
 
-  vector<char> test3 = { card::FOREST, card::BLOOD_MOON };
+  vector<card> test3 = { card::FOREST, card::BLOOD_MOON };
   (verifyHand(test3) == false) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "forest + blood moon\n";
 
-  vector<char> test4 = { card::UTOPIA_SPRAWL, card::BLOOD_MOON };
+  vector<card> test4 = { card::UTOPIA_SPRAWL, card::BLOOD_MOON };
   (verifyHand(test4) == false) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "utopia sprawl + blood moon\n";
 
-  vector<char> test5 = { card::FOREST, card::ARBOR_ELF, card::FOREST, card::BLOOD_MOON };
+  vector<card> test5 = { card::FOREST, card::ARBOR_ELF, card::FOREST, card::BLOOD_MOON };
   (verifyHand(test5) == false) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "arbor elf + blood moon + forest + forest\n";
 
-  vector<char> test6 = { card::ARBOR_ELF, card::PILLAGE, card::FOREST, card::MOUNTAIN };
+  vector<card> test6 = { card::ARBOR_ELF, card::PILLAGE, card::FOREST, card::MOUNTAIN };
   (verifyHand(test6) == false) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "arbor elf + pillage + forest + mountain\n";
 
-  vector<char> test7 = { card::ARBOR_ELF, card::PILLAGE, card::FOREST, card::FETCHLAND };
+  vector<card> test7 = { card::ARBOR_ELF, card::PILLAGE, card::FOREST, card::FETCHLAND };
   (verifyHand(test7) == true) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "arbor elf + pillage + forest + fetchland\n";
 
-  vector<char> test8 = { card::ARBOR_ELF, card::BIRDS_OF_PARADISE, card::PILLAGE, card::STOMPING_GROUND };
+  vector<card> test8 = { card::ARBOR_ELF, card::BIRDS_OF_PARADISE, card::PILLAGE, card::STOMPING_GROUND };
   (verifyHand(test8) == false) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "arbor elf + birds + pillage + stomping ground\n";
 
-  vector<char> test9 = { card::ARBOR_ELF, card::MAGUS_OF_THE_MOON, card::FETCHLAND, card::FETCHLAND };
+  vector<card> test9 = { card::ARBOR_ELF, card::MAGUS_OF_THE_MOON, card::FETCHLAND, card::FETCHLAND };
   (verifyHand(test9) == true) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "arbor elf + magus + fetchland + fetchland\n";
 
-  vector<char> test10 = { card::UTOPIA_SPRAWL, card::PILLAGE, card::STOMPING_GROUND, card::STOMPING_GROUND };
+  vector<card> test10 = { card::UTOPIA_SPRAWL, card::PILLAGE, card::STOMPING_GROUND, card::STOMPING_GROUND };
   (verifyHand(test10) == true) ? (cout << "ok: ") : (cout << "fail: ");
   cout << "utopia sprawl + pillage + stomping ground + stomping ground\n";
 
@@ -166,19 +169,20 @@ void runTests()
 int main()
 {
   srand(unsigned(time(0)));
+  Configuration config;
 
   // runTests();
 
-  vector<char> deck = initializeDeck();
-  vector<char> hand;
+  vector<card> deck = initializeDeck(config.DECKLIST, config.DECK_SIZE);
+  vector<card> hand;
   int goodHands = 0;
-  for (int i=0; i < NUM_TRIALS; i++) {
+  for (int i=0; i < config.NUM_TRIALS; i++) {
     int countMulligan = 0;
-    while (countMulligan <= MAX_MULLIGANS) {
+    while (countMulligan <= config.MAX_MULLIGANS) {
       random_shuffle( deck.begin(), deck.end() );
 
       // Draw cards
-      for (int i= 0; i < STARTING_HAND_SIZE - countMulligan; i++) {
+      for (int i= 0; i < config.STARTING_HAND_SIZE - countMulligan; i++) {
         hand.push_back(deck[i]);
       }
 
@@ -194,8 +198,8 @@ int main()
     }
   }
 
-  double percent = 100.0 * goodHands / NUM_TRIALS;
-  cout << goodHands << '/' << NUM_TRIALS << " games with good starting hands. (" << percent << "%)";
+  double percent = 100.0 * goodHands / config.NUM_TRIALS;
+  cout << goodHands << '/' << config.NUM_TRIALS << " games with good starting hands. (" << percent << "%)";
 
   return 0;
 }
